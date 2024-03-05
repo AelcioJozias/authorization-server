e060c091da0c5cd628890ad9e28045cab015d46c
AelcioJozias <aelciojoziasputzel18@gmail.com>
2024-03-04 21:09:27 -0300

    artefatos inicias do projeto

---
+++ src/main/java/com/trainingapi/trainingauth/TrainingAuthApplication.java
@@ -0,0 +1,13 @@
+package com.trainingapi.trainingauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingAuthApplication.class, args);
+	}
+
+}
